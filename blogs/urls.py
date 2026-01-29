ebfd34f030cc4722215c1810fbbc770b5315a2c3
Digvijay7310 <kumardigvijay377@gmail.com>
2026-01-29 13:48:35 +0530

    add blog detail page

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<int:category_id>/', views.posts_by_category , name="posts_by_category"),
-    # path('<int:blog_id>/', views.blog_detail , name="blog_detail"),
+    path('<slug:slug>/', views.blog_detail , name="blog_detail"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -5,21 +5,36 @@ from .models import Blog, Category
 # Create your views here.
 def posts_by_category(request, category_id):
 
-    try:
-        category = get_object_or_404(Category, id=category_id)
-    except:
-        return redirect('home')
+    category = get_object_or_404(Category, id=category_id)
 
     # Fetch the posts that belongs to the category with the id category id
     posts = Blog.objects.filter(status="Publish", category=category)
 
     context = {
-        'category': category,
-        'posts': posts
+        'posts': posts,
     }
     return render(request, 'posts_by_category.html', context)
 
+def blog_detail(request, slug):
+    post = get_object_or_404(Blog, slug=slug)
 
-# def blog_detail(request, blog_id):
-#     blog = get_object_or_404(Blog, pk=blog_id)
-#     return render(request, 'blog_detail.html', {'blog_detail': blog})
+    # First: same category, exclude current post
+    related_posts = Blog.objects.filter(
+        category=post.category,
+        status="Publish"
+    ).exclude(id=post.id)[:4]
+
+    # Fallback: if not enough posts, get latest ones
+    if related_posts.count() < 4:
+        extra_posts = Blog.objects.filter(
+            status="Publish"
+        ).exclude(id__in=[p.id for p in related_posts]).exclude(id=post.id)[:4 - related_posts.count()]
+
+        related_posts = list(related_posts) + list(extra_posts)
+
+    context = {
+        'post': post,
+        'related_posts': related_posts,
+    }
+
+    return render(request, 'blog_detail.html', context)
